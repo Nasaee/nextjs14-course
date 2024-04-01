8384d32be1574a98e35332c0978ff5626d203181
nasaee_window <nasaee.dev@gmail.com>
2024-04-01 12:55:38 +0700

    Image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Header from '@/components/Header';
 import Logo from '@/components/Header/Logo';
+import Image from 'next/image';
+import programmingImg from '../../public/programming.jpg';
 
 export default function Home() {
   return (
     <div>
-      <Header name={'Kevin'} age={31} isMan={true}>
-        <Logo />
-      </Header>
+      <div className='w-[500px]'>
+        <Image quality={1} src={programmingImg} alt='programming' />
+      </div>
     </div>
   );
 }
